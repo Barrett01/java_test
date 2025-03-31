4c5273199e205e8142144d81fa6e70e7ba37e81e
hh <2726205591@qq.com>
2025-04-01 00:28:33 +0800

    分析一下LinkedHashSet的底层源码和备注

--- test001/src/TestJiHe/LinkedHashSetSource.java
+++ test001/src/TestJiHe/LinkedHashSetSource.java
@@ -13,5 +13,12 @@ public class LinkedHashSetSource {
         linkedHashSet.add(1);
         linkedHashSet.add("hep");
         System.out.println(linkedHashSet);
+        /*
+        1，加入和取出顺序一致
+        2，底层维护的是一个LinkedHashMap
+        3，底层结构（数据+双向链表）
+        4，添加第一次，直接将table扩容到16 存放的节点类型为linkedhashmap$entry
+        5,数组是HashMap$Node[]存放的元素/数据是linkedhashmap$entry类型
+         */
     }
 }
