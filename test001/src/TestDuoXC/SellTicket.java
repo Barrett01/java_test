116e2c2d2fe9a6394e84262dfe111fe82ce04537
hh <2726205591@qq.com>
2025-04-20 16:57:43 +0800

    使用synchronized 实现多窗口售票的同步{线程同步}

--- test001/src/TestDuoXC/SellTicket.java
+++ test001/src/TestDuoXC/SellTicket.java
@@ -1,4 +1,5 @@
 package TestDuoXC;
+
 /*
 模拟三个窗口售票
 1,会出现多卖的情况
@@ -6,31 +7,41 @@ package TestDuoXC;
 public class SellTicket {
     public static void main(String[] args) {
         SellTicket01 sellTicket01 = new SellTicket01();
-        SellTicket01 sellTicket02 = new SellTicket01();
-        SellTicket01 sellTicket03 = new SellTicket01();
+//        SellTicket01 sellTicket02 = new SellTicket01(); //不能这样写
+//        SellTicket01 sellTicket03 = new SellTicket01();
 
         new Thread(sellTicket01).start();
-        new Thread(sellTicket02).start();
-        new Thread(sellTicket03).start();
+        new Thread(sellTicket01).start();
+        new Thread(sellTicket01).start();
 
     }
 }
-class SellTicket01 implements Runnable{
+
+class SellTicket01 implements Runnable {
     private static int ticket = 100;
+    private boolean loop = true;
+
+    public synchronized void sell() {
+        if (ticket <= 0) {
+            System.out.println("票已售尽");
+            loop = false;
+            return;
+        }
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+//        --ticket;
+        System.out.println("窗口：" + Thread.currentThread().getName() + "售出一张票 剩余票数：" + (--ticket));
+
+    }
+
     @Override
     public void run() {
-
-        while(true){
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            ticket--;
-            System.out.println("还有多少票："+ticket);
-            if(ticket <= 0){
-                break;
-            }
+        while (loop) {
+            sell();
         }
     }
 }
