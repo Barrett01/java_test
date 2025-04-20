b00080912a8ef76bf1f6730177b06c29233a3e17
hh <2726205591@qq.com>
2025-04-20 23:50:58 +0800

    练习-互斥锁2

--- test001/src/TestDuoXC/XCHomeWork02.java
+++ test001/src/TestDuoXC/XCHomeWork02.java
@@ -1,4 +1,45 @@
 package TestDuoXC;
 
 public class XCHomeWork02 {
+    public static void main(String[] args) {
+        CCka cCka = new CCka();
+        Thread thread1 = new Thread(cCka);
+        thread1.setName("Thread1");
+        Thread thread2 = new Thread(cCka);
+        thread2.setName("Thread2");
+        thread1.start();
+        thread2.start();
+    }
+}
+
+class CCka implements Runnable {
+    private boolean loop = true;
+    private int Amount = 10000;
+
+    public /*synchronized*/ void wallet() {
+        synchronized (this) {
+
+            if (Amount < 1000) {
+                loop = false;
+                System.out.println("余额不足");
+                return;
+            }
+            Amount = Amount - 1000;
+            System.out.println(Thread.currentThread().getName() + "取出1000" + "还剩余额：" + Amount);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    @Override
+    public void run() {
+        while (loop) {
+            wallet();
+        }
+    }
 }
