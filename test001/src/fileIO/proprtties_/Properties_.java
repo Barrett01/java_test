948481e63c2c7af1afe542edb29581600ce0a145
hh <2726205591@qq.com>
2025-04-25 07:14:21 +0800

    properties的常用方法

---
+++ test001/src/fileIO/proprtties_/Properties_.java
@@ -0,0 +1,17 @@
+package fileIO.proprtties_;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Properties_ {
+    public static void main(String[] args) throws IOException {
+        Properties prop = new Properties();
+        prop.load(new FileReader("D:\\githubCount\\test\\java_test\\test001\\src\\mysql.properties"));
+        System.out.println(prop.getProperty("user"));
+        System.out.println(prop.getProperty("pws"));
+        prop.list(System.out);
+        
+    }
+}
